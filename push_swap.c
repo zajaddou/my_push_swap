07e832af3ef804c43f8964b3000017e09284a688
Zakaria Ajaddou <54480895+zajaddou@users.noreply.github.com>
2025-03-14 23:54:27 +0000

    update

--- push_swap.c
+++ push_swap.c
@@ -1,8 +1,14 @@
 
 #include "push_swap.h"
 
+void leak()
+{
+    system("leaks push_swap");
+}
+
 int main(int ac, char *av[])
 {
+	atexit(leak);
 	t_stack *stack_a = NULL;
 
 	int *list = parsing(ac, av);
